f1f23259a55c126a4ef2d7d256c7e6a1741d2f21
Sam T <samtny@gmail.com>
2014-10-11 02:26:39 +0000

    folders

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 
 require_once 'system/deliver.php';
 require_once 'system/resource.php';
+require_once 'system/folder.php';
 
 $request_uri = $_SERVER['REQUEST_URI'];
 $params = explode("/", trim($request_uri, '/'));
 
 switch ($params[0]) {
+  case "folder_list":
+    $parent = isset($params[1]) ? $params[1] : "";
+    $start = isset($params[2]) ? $params[2] : 0;
+    $count = isset($params[3]) ? $params[3] : FOLDER_PAGE;
+    $sort = !empty($params[4]) ? $params[4] : FOLDER_SORT;
+
+    $folder_list = folder_list($parent, $start, $count, $sort);
+    deliver_json($folder_list);
+
+    break;
+
   case "resource_list":
     $start = isset($params[1]) ? $params[1] : 0;
     $count = isset($params[2]) ? $params[2] : RESOURCE_PAGE;
---
+++ system/folder.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'config.php';
+
+function folder_get($folder_id) {
+  $folder = NULL;
+
+  $m = new MongoClient(MONGO_URI);
+  $db = $m->{MONGO_DB};
+  $collection = new MongoCollection($db, 'folders');
+
+  $folders_query = array('_id' => new MongoId($folder_id));
+  $folders_fields = array('filename', 'key');
+
+  $folder = $collection->findOne($folders_query, $folders_fields);
+
+  return $folder;
+}
+
+function folder_list($parent = "", $start = 0, $count = FOLDER_PAGE, $sort = FOLDER_SORT) {
+  $folder_list = array();
+
+  $m = new MongoClient(MONGO_URI);
+  $db = $m->{MONGO_DB};
+  $collection = new MongoCollection($db, 'folders');
+  
+  $folders_query = array('parent' => $parent);
+  $folders_fields = array('name', 'parent');
+
+  $cursor = $collection->find($folders_query, $folders_fields)->sort(array($sort => 1))->skip($start)->limit($count);
+
+  while ($object = $cursor->getNext()) {
+    $folder_list[] = $object;
+  }
+
+  return $folder_list;
+}
+
